205d84d00085635f4a9ad60260721cfa6a637589
Ritikesh <ritikeshsisodiya@gmail.com>
2023-02-06 22:44:43 +0530

    rubocop V1

--- lib/blueprinter/extractors/block_extractor.rb
+++ lib/blueprinter/extractors/block_extractor.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
+require 'pry'
 module Blueprinter
   # @api private
   class BlockExtractor < Extractor
-    def extract(field_name, object, local_options, options = {})
+    def extract(_field_name, object, local_options, options = {})
       options[:block].call(object, local_options)
     end
   end
